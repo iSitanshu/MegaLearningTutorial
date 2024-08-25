17a0318d78072829043a9f9b065a953e5f56c93b
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-25 23:40:38 +0530

    Mega

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -7,8 +7,6 @@ import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
-
-
 import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
